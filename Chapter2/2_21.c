b6f99702bad7205e46d1e2d6a6ae7f95450b4d17
SulemanAwais <sulemanawais2001@gmail.com>
2023-12-24 12:02:16 +0500

    2_21

---
+++ Chapter2/2_21.c
@@ -0,0 +1,15 @@
+/*
+(Shapes with Asterisks) Write a program that prints the following shapes with asterisks
+see 2_21.PNG
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("*********\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*********\n\n");
+    printf("    ***    \n * \t * \n*\t  *\n*\t  *\n*\t  *\n*\t  *\n*\t  *\n * \t * \n    ***    \n\n");
+    printf("  *  \n *** \n*****\n  *  \n  *  \n  *  \n  *  \n  *  \n  *  \n\n");
+    printf("    *    \n   * *   \n  *   *  \n *     * \n*       *\n *     * \n  *   *  \n   * *   \n    *    \n");
+    return 0;
+
+}

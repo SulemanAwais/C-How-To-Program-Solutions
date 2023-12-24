860ea52ff6a556a5bb398e992d2787b46072e802
SulemanAwais <sulemanawais2001@gmail.com>
2023-12-24 12:07:19 +0500

    2_27

---
+++ Chapter2/2_27.c
@@ -0,0 +1,20 @@
+/*
+(Checkerboard Pattern of Asterisks) 
+Display the following checkerboard pattern with eight printf statements and then display the same pattern with as few printf statements as possible.
+(see 2_27.PNG to checkout the pattern )
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("* * * * * * * * \n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * * \n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * * \n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * * \n");
+    printf(" * * * * * * * *\n\n");
+    printf("* * * * * * * * \n * * * * * * * *\n* * * * * * * * \n * * * * * * * *\n* * * * * * * * \n * * * * * * * *\n* * * * * * * * \n * * * * * * * *\n");
+    return 0;
+}

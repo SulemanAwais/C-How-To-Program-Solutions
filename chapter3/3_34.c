fb1f83f90fa39b69c0075a9380766ae2da175179
SulemanAwais <sulemanawais2001@gmail.com>
2023-12-25 22:11:52 +0500

    3_34

---
+++ chapter3/3_34.c
@@ -0,0 +1,23 @@
+/*
+(Floyd's Triangle) 
+Floyd’s Triangle is a right-angled triangular array of natural numbers. It
+is defined by filling rows with consecutive integers. Thus, row 1 will have the number 1, row 2 will
+have the numbers 2 and 3, and so on. Write a program that draws a 10-line Floyd’s triangle. An
+outer loop can control the number of lines to be printed and an inner loop can ensure that each row
+contains the correct number of integers
+*/
+#include <stdio.h>
+int main()
+{
+    int count=1;
+    for(int i=1;i<=10;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d,",count);
+            count++;
+        }
+        printf("\n");
+    }
+    return 0;
+}

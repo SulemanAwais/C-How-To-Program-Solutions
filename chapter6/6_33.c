2290b9645ec271324a3a44366b53d81b05336bb3
SulemanAwais <sulemanawais2001@gmail.com>
2023-12-28 22:12:27 +0500

    6_33 problem_statement

--- chapter6/6_33.c
+++ chapter6/6_33.c
@@ -1,4 +1,11 @@
+/*
+(Binary Search) Modify the program of Fig. 6.19 to use a recursive binarySearch function
+to perform the binary search of the array. The function should receive an integer array, the starting
+index, the ending index and the search key as arguments. If the search key is found, return the array
+index; otherwise, return –1
+*/
 #include <stdio.h>
+
 int binarySearch(int arr[], int start, int end, int key) {
     if (start <= end) {
         int mid = start + (end - start) / 2;

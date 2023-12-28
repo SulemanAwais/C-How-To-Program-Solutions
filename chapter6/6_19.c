4406b4c10daf3d953a0f8407e979192fea36d22f
SulemanAwais <sulemanawais2001@gmail.com>
2023-12-28 22:06:32 +0500

    6_19 problem_statement

--- chapter6/6_19.c
+++ chapter6/6_19.c
@@ -1,3 +1,14 @@
+/*
+(Dice Rolling) 
+Write a program that simulates the rolling of two dice. The program should
+use rand twice to roll the first die and second die, respectively. The sum of the two values should
+then be calculated. [Note: Because each die can show an integer value from 1 to 6, then the sum of
+the two values will vary from 2 to 12, with 7 being the most frequent sum and 2 and 12 the least
+frequent sums.] Figure 6.24 shows the 36 possible combinations of the two dice. Your program
+should roll the two dice 36,000 times. Use a one-dimensional array to tally the numbers of times
+each possible sum appears. Print the results in a tabular format. Also, determine if the totals are reasonable;
+i.e., there are six ways to roll a 7, so approximately one-sixth of all the rolls should be 7.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>

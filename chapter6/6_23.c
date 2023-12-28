af27393a1b58713711990bde5ef9014a65b2f60f
SulemanAwais <sulemanawais2001@gmail.com>
2023-12-28 22:11:10 +0500

    6_23 problem_statement

--- chapter6/6_23.c
+++ chapter6/6_23.c
@@ -1,4 +1,36 @@
+/*
+(Turtle Graphics) 
+The Logo language made the concept of turtle graphics famous. Imagine
+a mechanical turtle that walks around the room under the control of a C program. The turtle holds
+a pen in one of two positions, up or down. While the pen is down, the turtle traces out shapes as it
+moves; while the pen is up, the turtle moves about freely without writing anything. In this problem
+you’ll simulate the operation of the turtle and create a computerized sketchpad as well. 
+Use a 50-by-50 array floor which is initialized to zeros. Read commands from an array that
+contains them. Keep track of the current turtle position at all times and whether the pen is currently up or down.
+Assume that the turtle always starts at position 0, 0 of the floor with its pen up.
+The set of turtle commands your program must process are shown in Fig. 6.25. Suppose that the
+turtle is somewhere near the center of the floor. The following “program” would draw and print a
+12-by-12 square:
+2
+5,12
+3
+5,12
+3
+5,12
+3
+5,12
+1
+6
+9
+As the turtle moves with the pen down, set the appropriate elements of array floor to 1s. When the
+6 command (print) is given, wherever there’s a 1 in the array, display an asterisk, or some other
+character you choose. Wherever there’s a zero, display a blank. Write a program to implement the
+turtle graphics capabilities discussed here. Write several turtle graphics programs to draw interesting shapes.
+Add other commands to increase the power of your turtle graphics language.
+
+*/
 #include <stdio.h>
+
 void displayFloor(int floor[50][50]) {
     for (int i = 0; i < 50; ++i) {
         for (int j = 0; j < 50; ++j) {
